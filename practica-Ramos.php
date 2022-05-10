ddd3ebee4cb9d15c88c997ea7208f4a23a039480
Cristian <47992376+Rameaux090@users.noreply.github.com>
2022-05-10 20:08:07 +0200

    Script PHP simple para tarea DAW

---
+++ practica-Ramos.php
@@ -0,0 +1,25 @@
+<?php
+    /**
+    *@author Cristian Ramos Rodriguez.
+    *@version 26/04/2022
+    *@param variable nombre de entrada que almacenará los datos que reciba.    *
+    */
+	function saludo($nombre) {
+		echo "Hola  $nombre, esto es un saludo de prueba \n";
+	}
+	//Invocamos la funcion saludo insertandole nuestro nombre.
+	saludo('Cristian');
+    
+    /**
+    * Función que calcula el resultado de la suma de dos números y lo muestra por pantalla.
+    *@param variable nombre de entrada que almacenará los datos que reciba.
+    *@return 37   
+    */	
+	function sumar($num1,$num2) {
+		$resultado = $num1 +$num2;
+		return $resultado;		
+	}
+	$resultado_suma = sumar(18,19);
+	echo  "El resultado de la suma de 18 y 19 son $resultado_suma \n";
+	
+?>
